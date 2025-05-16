90d710af22cc4a1b516c0230724223b6e9527ca0
Abinayah12 <abinayahravichandran@gmail.com>
2025-05-16 12:42:53 +0530

    FeatureAdd v2.0

--- Simple Contact Manager/Form1.Designer.cs
+++ Simple Contact Manager/Form1.Designer.cs
@@ -34,14 +34,19 @@
             this.txtPhone = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(258, 32);
+            this.btnAdd.Location = new System.Drawing.Point(408, 239);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(96, 43);
+            this.btnAdd.Size = new System.Drawing.Size(108, 41);
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -50,36 +55,40 @@
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(98, 29);
+            this.txtName.Location = new System.Drawing.Point(254, 37);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(154, 30);
+            this.txtName.Size = new System.Drawing.Size(204, 36);
             this.txtName.TabIndex = 1;
             // 
             // lstDisplay
             // 
             this.lstDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstDisplay.FormattingEnabled = true;
-            this.lstDisplay.ItemHeight = 25;
-            this.lstDisplay.Location = new System.Drawing.Point(98, 135);
+            this.lstDisplay.ItemHeight = 29;
+            this.lstDisplay.Location = new System.Drawing.Point(176, 297);
+            this.lstDisplay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstDisplay.Name = "lstDisplay";
-            this.lstDisplay.Size = new System.Drawing.Size(256, 229);
+            this.lstDisplay.Size = new System.Drawing.Size(340, 265);
             this.lstDisplay.TabIndex = 2;
             // 
             // txtPhone
             // 
             this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhone.Location = new System.Drawing.Point(98, 77);
+            this.txtPhone.Location = new System.Drawing.Point(254, 112);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(154, 30);
+            this.txtPhone.Size = new System.Drawing.Size(204, 36);
             this.txtPhone.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Location = new System.Drawing.Point(68, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.Size = new System.Drawing.Size(81, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "Name";
             // 
@@ -87,27 +96,66 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(7, 80);
+            this.label2.Location = new System.Drawing.Point(68, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 25);
+            this.label2.Size = new System.Drawing.Size(86, 29);
             this.label2.TabIndex = 3;
             this.label2.Text = "Phone";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(292, 239);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(176, 239);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(128, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(472, 191);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Details";
+            // 
             // frmContacts
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(362, 374);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(690, 575);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lstDisplay);
-            this.Controls.Add(this.txtPhone);
-            this.Controls.Add(this.txtName);
             this.Controls.Add(this.btnAdd);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmContacts";
             this.Text = "Contact";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -119,6 +167,9 @@
         private System.Windows.Forms.TextBox txtPhone;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Simple Contact Manager/Form1.cs
+++ Simple Contact Manager/Form1.cs
@@ -19,12 +19,60 @@ namespace Simple_Contact_Manager
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            string name = txtName.Text;
             string phone = txtPhone.Text;
 
-            // Bug: Doesn't check if fields are empty
-            // Bug: Accepts invalid phone number
-            lstDisplay.Items.Add(name + " - " + phone);
+            if (!string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                if (!string.IsNullOrWhiteSpace(txtPhone.Text))
+                {
+                    if (
+ (phone.StartsWith("+") && phone.Length >= 11 && phone.Length <= 13 && phone.Substring(1).All(char.IsDigit)) ||
+ (phone.Length >= 10 && phone.Length <= 12 && phone.All(char.IsDigit))
+)
+                    {
+                        string name = txtName.Text;
+
+                        // Bug: Doesn't check if fields are empty
+                        // Bug: Accepts invalid phone number
+                        lstDisplay.Items.Add(name + " - " + phone);
+
+                        txtPhone.Text = "";
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Phone number must be between 10 to 12 digits and contain only numbers");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Phone Canot be Null");
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Name Canot be Null");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtName.Text="";
+            txtPhone.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(lstDisplay.SelectedItem != null)
+            {
+                lstDisplay.Items.Remove(lstDisplay.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Select a contact to delete");
+            }
         }
     }
 }
